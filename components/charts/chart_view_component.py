b62069cdd170707807e700fd9d913630f1a43b13
PavelZakh <ya.pahan102@yandex.ru>
2026-07-12 17:55:39 +0500

    Добавление Allure шагов в автотесты. Практика работы с allure.step

--- components/charts/chart_view_component.py
+++ components/charts/chart_view_component.py
@@ -1,6 +1,7 @@
 from enum import StrEnum
 
-from playwright.sync_api import Page, expect
+import allure
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
 from elements.image import Image
@@ -34,6 +35,7 @@ class ChartViewComponent(BaseComponent):
         self.title = Text(page, f'{identifier}-widget-title-text', 'Title')
         self.chart = Image(page, f'{identifier}-{chart_type}-chart', 'Chart')
 
+    @allure.step('Check visible "{title}" chart')
     def check_visible(self, title: str):
         self.title.check_visible()
         self.title.check_have_text(title)
