6277b99a97aeba392942284b3f2a1fe5080039e9
PavelZakh <ya.pahan102@yandex.ru>
2026-07-12 15:28:56 +0500

    Интеграция Allure в автотесты. Практическая работа с Allure аннотациями

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,12 +1,27 @@
 import pytest
+import allure
+from allure_commons.types import Severity
 
 from pages.courses.create_course_page import CreateCoursePage
 from pages.courses.courses_list_page import CoursesListPage
+from tools.allure.tags import AllureTag
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
 
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.tag(AllureTag.REGRESSION, AllureTag.COURSES)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.COURSES)
+@allure.story(AllureStory.COURSES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.COURSES)
+@allure.sub_suite(AllureStory.COURSES)
 class TestCourses:
+    @allure.title("Check displaying of empty courses list")
+    @allure.severity(Severity.NORMAL)
     def test_empty_courses_list(self, courses_list_page_with_state: CoursesListPage) -> None:
         courses_list_page_with_state.visit_courses_list_page()
 
@@ -17,7 +32,8 @@ class TestCourses:
 
         courses_list_page_with_state.check_visible_empty_view()
 
-
+    @allure.title("Create course")
+    @allure.severity(Severity.CRITICAL)
     def test_create_course(
             self, create_course_page_with_state: CreateCoursePage, courses_list_page_with_state: CoursesListPage
     ) -> None:
@@ -50,6 +66,8 @@ class TestCourses:
             index=0, title='Playwright', max_score='100', min_score='10', estimated_time='2 weeks'
         )
 
+    @allure.title("Edit course")
+    @allure.severity(Severity.CRITICAL)
     def test_edit_course(
             self, create_course_page_with_state: CreateCoursePage, courses_list_page_with_state: CoursesListPage
     ) -> None:
