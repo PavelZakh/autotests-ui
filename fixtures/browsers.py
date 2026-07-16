8299725ee90f024dd2beaea6ba2d3b9f7bfa2e0f
PavelZakh <ya.pahan102@yandex.ru>
2026-07-16 22:10:42 +0500

    Добавление Playwright Trace Viewer в Allure отчет. Прикрепляем отчет Playwright Tracing к Allure отчету

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,27 +1,18 @@
 from typing import Any, Generator
 
-import allure
 import pytest
 from _pytest.fixtures import SubRequest
 from playwright.sync_api import Playwright, Page
 
 from pages.authentication.registration_page import RegistrationPage
+from tools.playwright.pages import initialize_playwright_page
 
 BROWSER_STATE_PATH: str = 'browser_state.json'
 
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Generator[Page, Any, None]:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-
-    yield context.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
 
 
 @pytest.fixture(scope="session")
@@ -40,17 +31,12 @@ def initialize_browser_state(playwright: Playwright) -> None:
     browser.close()
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def chromium_page_with_state(
         initialize_browser_state: None, request: SubRequest, playwright: Playwright
 ) -> Generator[Page, Any, None]:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state=BROWSER_STATE_PATH)
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-
-    yield context.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        storage_state=BROWSER_STATE_PATH
+    )
